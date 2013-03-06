06326eace51d9f427804155323204ec8b946e6fc
efruchter <fruchter.eric@gmail.com>
2013-03-06 04:25:28 -0500

    better comments

--- pylens.py
+++ pylens.py
@@ -2,17 +2,24 @@ from Tkinter import *
 import webbrowser
 
 root = Tk()
-root.title("Pyfred")
+root.title("Pylens")
 
 text = Text(root)
 text.config(width=60, height=1)
 text.pack(side=LEFT, fill=Y)
 
+# core callback to handle the user query
 def callback(event):
-  url = text.get(1.0, END).strip()
-  webbrowser.open(url, 2, True)
-  root.destroy()
+	query = text.get(1.0, END).strip()
 
+	# analyze the query and do something awesome
+	if len(query) > 0:
+		webbrowser.open(query, 2, True)
+
+	# nicely close down
+	root.destroy()
+
+# bind callback and steal focus
 text.bind("<Return>", callback)
 text.focus_force()
 
@@ -24,4 +31,5 @@ y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2
 root.geometry("+%d+%d" % (x, y))
 root.deiconify()
 
+# go time!
 root.mainloop()
