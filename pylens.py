5bef7e5d5d77f3e8e9b65ee96933382b22ebd7c5
Patrick Lucas <me@patricklucas.com>
2013-03-07 10:41:49 -0500

    Refactor/pythonicize all the things

--- pylens.py
+++ pylens.py
@@ -1,74 +1,94 @@
-from Tkinter import *
+import os
+from Tkinter import END, LEFT, Tk, Text, Y
+import subprocess
 import webbrowser
-from subprocess import Popen as runCommand
-from os.path import expanduser
 
-root = Tk()
-root.title("pylens")
+QUERY_SWAP_TOKEN = "{query}"
+CONFIG_PATH = os.path.expanduser("~/.pylens.conf")
 
-text = Text(root)
-text.config(width=60, height=1)
-text.pack(side=LEFT, fill=Y)
 
-# custom searches
-lenses = []
-querySwapToken = "{query}"
-home = expanduser("~");
+class PyLens(object):
 
-# parse the config
-try:
-	configs = open(home + "/pylens.conf", 'r')
-	for lens in configs:
-		if not lens.startswith("#") and len(lens.strip()) > 0:
-			sep = lens.index(" ")
-			lenses += [{"command" : lens[:sep].strip(), "query" : lens[sep:].strip()}]
-except IOError:
-	print "no config file '~/pylens.conf' found"
+    def __init__(self):
+        self.root, self.text = self._init_ui()
+        self.lenses = self._init_lenses()
 
-# core callback to handle the user query
-def handleQuery(event):
-	query = text.get(1.0, END).strip()
+    def _init_ui(self):
+        root = Tk()
+        root.title("pylens")
+        root.bind("<FocusOut>", self.close)
+        root.bind("<Escape>", self.close)
 
-	# try to match a macro
-	bestLens = None
-	for lens in lenses:
-		if (query + " ").startswith(lens["command"] + " "):
-			if bestLens is None or len(lens["command"]) > len(bestLens["command"]):
-				bestLens = lens
-	if bestLens is not None:
-		query = bestLens["query"].replace(querySwapToken, query[len(bestLens["command"]):].strip())
+        # center the window
+        root.withdraw()
+        root.update_idletasks() # Update "requested size" from geometry manager
+        x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 2
+        y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2
+        root.geometry("+%d+%d" % (x, y))
+        root.deiconify()
 
-	# 'o' is the lens for running a command
-	if query.startswith("o "):
-		query = query[2:].strip().replace("~", home)
-		runCommand(query, shell=True)
-	# no lens means the query is a website or a search
-	elif len(query) > 0:
-		if "." in query and " " not in query and not query.startswith("http"):
-			query = "http://" + query
-		elif not query.startswith("http"):
-			query = "https://www.google.com/search?q=" + query
-		webbrowser.open(query, 2, True)
+        text = Text(root)
+        text.config(width=60, height=1)
+        text.pack(side=LEFT, fill=Y)
+        text.bind("<Return>", self.handle_query)
+        text.focus_force()
 
-	close(event)
+        return root, text
 
-# close down nicely
-def close(event):
-	root.destroy()
+    def _init_lenses(self):
+        lenses = [] # Custom searches
 
-root.bind("<FocusOut>", close)
-root.bind("<Escape>", close)
+        # Parse the config file
+        if os.path.exists(CONFIG_PATH):
+            with open(CONFIG_PATH) as f:
+                for line in f:
+                    line = line.strip()
 
-text.bind("<Return>", handleQuery)
-text.focus_force()
+                    if not line or line.startswith('#'):
+                        continue
 
-# center the window
-root.withdraw()
-root.update_idletasks()  # Update "requested size" from geometry manager
-x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 2
-y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2
-root.geometry("+%d+%d" % (x, y))
-root.deiconify()
+                    command, query = line.split(' ', 1)
+                    self.lenses.append(dict(command=command, query=query))
 
-# go time!
-root.mainloop()
+        return lenses
+
+    def run(self):
+        self.root.mainloop()
+
+    def handle_query(self, event):
+        query = self.text.get(1.0, END).strip()
+
+        # try to match a macro
+        best_lens = None
+        for lens in self.lenses:
+            if (query + " ").startswith(lens['command']):
+                if (not best_lens or
+                      len(lens['command']) > len(best_lens['command'])):
+                    best_lens = lens
+
+        if best_lens is not None:
+            query = best_lens["query"].replace(QUERY_SWAP_TOKEN,
+                query[len(best_lens["command"]):].strip())
+
+        if query.startswith("o "):
+            # 'o' is the lens for running a command
+            query = query[2:].strip().replace("~", os.path.expanduser("~"))
+            subprocess.call(query, shell=True)
+        elif len(query) > 0:
+            # no lens means the query is a website or a search
+            if ("." in query and " " not in query and
+                  not query.startswith("http")):
+                query = "http://" + query
+            elif not query.startswith("http"):
+                query = "https://www.google.com/search?q=" + query
+
+            webbrowser.open(query, 2, True)
+
+        self.close(event)
+
+    def close(self, event):
+        self.root.destroy()
+
+
+if __name__ == '__main__':
+    PyLens().run()
